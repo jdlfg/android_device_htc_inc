092ff686b42295a93db93a68c0c70197e5011c44
cvpcs <root@cvpcs.org>
2010-07-08 11:16:57 -0500

    so much cleanup... guaranteed to be broken as hell

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -1,35 +1,50 @@
-# Copyright (C) 2009 The Android Open Source Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-#
-# AndroidBoard.mk is a legacy mechanism to deal with a few
-# edge-cases that can't be managed otherwise. No new rules
-# should be added to this file.
-#
-
 LOCAL_PATH := $(call my-dir)
 
-# Least specific includes go first, so that they can get
-# overridden further down
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/kernel/zImage
+endif
 
-$(call add-radio-file,recovery/images/firmware_install.565)
-$(call add-radio-file,recovery/images/firmware_error.565)
-$(call add-radio-file,recovery/images/bitmap_size.txt)
+file := $(INSTALLED_KERNEL_TARGET)
+ALL_PREBUILT += $(file)
+$(file): $(TARGET_PREBUILT_KERNEL) | $(ACP)
+	$(transform-prebuilt-to-target)
+
+file := $(TARGET_ROOT_OUT)/init.inc.rc
+ALL_PREBUILT += $(file)
+$(file) : $(LOCAL_PATH)/init.inc.rc | $(ACP)
+	$(transform-prebuilt-to-target)
+
+ifeq ($(USE_HTC_PROPRIETARIES),true)
+include $(CLEAR_VARS)
+LOCAL_MODULE := libgps.so
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
+LOCAL_SRC_FILES := proprietary/$(LOCAL_MODULE)
+OVERRIDE_BUILT_MODULE_PATH := $(TARGET_OUT_INTERMEDIATE_LIBRARIES)
+include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := libaudiopolicygeneric.so
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
+LOCAL_SRC_FILES := proprietary/$(LOCAL_MODULE)
+OVERRIDE_BUILT_MODULE_PATH := $(TARGET_OUT_INTERMEDIATE_LIBRARIES)
+include $(BUILD_PREBUILT)
 
-ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
+include $(CLEAR_VARS)
+LOCAL_MODULE := libcamera.so
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
+LOCAL_SRC_FILES := proprietary/$(LOCAL_MODULE)
+OVERRIDE_BUILT_MODULE_PATH := $(TARGET_OUT_INTERMEDIATE_LIBRARIES)
+include $(BUILD_PREBUILT)
 
-# include the non-open-source counterpart to this file
--include vendor/htc/inc/AndroidBoardVendor.mk
+include $(CLEAR_VARS)
+LOCAL_MODULE := libhtc_ril.so
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
+LOCAL_SRC_FILES := proprietary/$(LOCAL_MODULE)
+OVERRIDE_BUILT_MODULE_PATH := $(TARGET_OUT_INTERMEDIATE_LIBRARIES)
+include $(BUILD_PREBUILT)
+
+endif
--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,32 +1,4 @@
-# Copyright (C) 2009 The Android Open Source Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-#
-# This file sets variables that control the way modules are built
-# thorughout the system. It should not be used to conditionally
-# disable makefiles (the proper mechanism to control what gets
-# included in a build is to use PRODUCT_PACKAGES in a product
-# definition file).
-#
-
-# WARNING: This line must come *before* including the proprietary
-# variant, so that it gets overwritten by the parent (which goes
-# against the traditional rules of inheritance).
 USE_CAMERA_STUB := true
-
-# inherit from the proprietary version
--include vendor/htc/inc/BoardConfigVendor.mk
 
 TARGET_NO_BOOTLOADER := true
 
@@ -69,8 +41,6 @@ BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x0f800000   # limited so we enforce room to
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x09500000
 BOARD_FLASH_BLOCK_SIZE := 131072
 
-TARGET_RECOVERY_UI_LIB := librecovery_ui_inc librecovery_ui_htc
-
-TARGET_RECOVERY_UPDATER_LIBS += librecovery_updater_htc
-
 TARGET_RELEASETOOLS_EXTENSIONS := device/htc/common
+
+BOARD_GPS_LIBRARIES := libgps
