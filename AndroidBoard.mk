3fb5de3055d7a3aeda54491593c0babeba0e4752
cvpcs <root@cvpcs.org>
2010-07-28 00:17:01 -0500

    updated to froyo leak for now

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -2,6 +2,12 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_PREBUILT_KERNEL),)
 TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/kernel/zImage
+
+# modules to include for default kernel
+PRODUCT_COPY_FILES += $(shell \
+        find $(LOCAL_PATH)/kernel -name '*.ko' \
+        | sed -r 's/^\/?(.*\/)([^/ ]+)$$/\1\2:system\/lib\/modules\/\2/' \
+        | tr '\n' ' ')
 endif
 
 file := $(INSTALLED_KERNEL_TARGET)
@@ -14,37 +20,72 @@ ALL_PREBUILT += $(file)
 $(file) : $(LOCAL_PATH)/init.inc.rc | $(ACP)
 	$(transform-prebuilt-to-target)
 
-ifeq ($(USE_HTC_PROPRIETARIES),true)
-include $(CLEAR_VARS)
-LOCAL_MODULE := libgps.so
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
-LOCAL_SRC_FILES := proprietary/$(LOCAL_MODULE)
-OVERRIDE_BUILT_MODULE_PATH := $(TARGET_OUT_INTERMEDIATE_LIBRARIES)
-include $(BUILD_PREBUILT)
+file := $(TARGET_ROOT_OUT)/bootcomplete.inc.rc
+ALL_PREBUILT += $(file)
+$(file) : $(LOCAL_PATH)/bootcomplete.inc.rc | $(ACP)
+	$(transform-prebuilt-to-target)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := libaudiopolicygeneric.so
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
-LOCAL_SRC_FILES := proprietary/$(LOCAL_MODULE)
-OVERRIDE_BUILT_MODULE_PATH := $(TARGET_OUT_INTERMEDIATE_LIBRARIES)
-include $(BUILD_PREBUILT)
+file := $(TARGET_ROOT_OUT)/shutdown.inc.rc
+ALL_PREBUILT += $(file)
+$(file) : $(LOCAL_PATH)/shutdown.inc.rc | $(ACP)
+	$(transform-prebuilt-to-target)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := libcamera.so
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
-LOCAL_SRC_FILES := proprietary/$(LOCAL_MODULE)
-OVERRIDE_BUILT_MODULE_PATH := $(TARGET_OUT_INTERMEDIATE_LIBRARIES)
-include $(BUILD_PREBUILT)
+ifneq ($(filter htc,$(USE_PROPRIETARIES)),)
+#include $(CLEAR_VARS)
+#LOCAL_MODULE := libmoto_gps.so
+#LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+#LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
+#LOCAL_SRC_FILES := proprietary/$(LOCAL_MODULE)
+#OVERRIDE_BUILT_MODULE_PATH := $(TARGET_OUT_INTERMEDIATE_LIBRARIES)
+#include $(BUILD_PREBUILT)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := libhtc_ril.so
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
-LOCAL_SRC_FILES := proprietary/$(LOCAL_MODULE)
-OVERRIDE_BUILT_MODULE_PATH := $(TARGET_OUT_INTERMEDIATE_LIBRARIES)
-include $(BUILD_PREBUILT)
+#include $(CLEAR_VARS)
+#LOCAL_MODULE := libaudio.so
+#LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+#LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
+#LOCAL_SRC_FILES := proprietary/$(LOCAL_MODULE)
+#OVERRIDE_BUILT_MODULE_PATH := $(TARGET_OUT_INTERMEDIATE_LIBRARIES)
+#include $(BUILD_PREBUILT)
+
+#include $(CLEAR_VARS)
+#LOCAL_MODULE := libaudiopolicy.so
+#LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+#LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
+#LOCAL_SRC_FILES := proprietary/$(LOCAL_MODULE)
+#OVERRIDE_BUILT_MODULE_PATH := $(TARGET_OUT_INTERMEDIATE_LIBRARIES)
+#include $(BUILD_PREBUILT)
+
+#include $(CLEAR_VARS)
+#LOCAL_MODULE := libcamera.so
+#LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+#LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
+#LOCAL_SRC_FILES := proprietary/$(LOCAL_MODULE)
+#OVERRIDE_BUILT_MODULE_PATH := $(TARGET_OUT_INTERMEDIATE_LIBRARIES)
+#include $(BUILD_PREBUILT)
+
+#include $(CLEAR_VARS)
+#LOCAL_MODULE := libril_rds.so
+#LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+#LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
+#LOCAL_SRC_FILES := proprietary/$(LOCAL_MODULE)
+#OVERRIDE_BUILT_MODULE_PATH := $(TARGET_OUT_INTERMEDIATE_LIBRARIES)
+#include $(BUILD_PREBUILT)
+
+#include $(CLEAR_VARS)
+#LOCAL_MODULE := libnmea.so
+#LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+#LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
+#LOCAL_SRC_FILES := proprietary/$(LOCAL_MODULE)
+#OVERRIDE_BUILT_MODULE_PATH := $(TARGET_OUT_INTERMEDIATE_LIBRARIES)
+#include $(BUILD_PREBUILT)
+
+#file := $(TARGET_OUT)/usr/keychars/qtouch-touchscreen.kcm.bin
+#ALL_PREBUILT += $(file)
+#$(file) : $(TARGET_OUT)/usr/keychars/qwerty.kcm.bin
+#	@echo "Symlink: $@ -> qwerty.kcm.bin"
+#	@mkdir -p $(dir $@)
+#	@rm -rf $@
+#	$(hide) ln -sf qwerty.kcm.bin $@
+#else
 
 endif
--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,15 +1,15 @@
-USE_CAMERA_STUB := true
-
-TARGET_NO_BOOTLOADER := true
+#
+# Product-specific compile-time definitions
 
 TARGET_BOARD_PLATFORM := qsd8k
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
-
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_GLOBAL_CFLAGS +=
+TARGET_GLOBAL_CPPFLAGS +=
 
-TARGET_BOOTLOADER_BOARD_NAME := inc
+TARGET_NO_BOOTLOADER := true
 
 # Wifi related defines
 BOARD_WPA_SUPPLICANT_DRIVER := WEXT
@@ -17,17 +17,20 @@ WPA_SUPPLICANT_VERSION      := VER_0_6_X
 BOARD_WLAN_DEVICE           := bcm4329
 WIFI_DRIVER_MODULE_PATH     := "/system/lib/modules/bcm4329.ko"
 WIFI_DRIVER_FW_STA_PATH     := "/system/etc/firmware/fw_bcm4329.bin"
-#WIFI_DRIVER_FW_AP_PATH      := "/system/etc/firmware/fw_bcm4329_apsta.bin"
+WIFI_DRIVER_FW_AP_PATH      := "/system/etc/firmware/fw_bcm4329_apsta.bin"
 WIFI_DRIVER_MODULE_ARG      := "firmware_path=/system/etc/firmware/fw_bcm4329.bin nvram_path=/proc/calibration"
 WIFI_DRIVER_MODULE_NAME     := "bcm4329"
 
-BOARD_USES_GENERIC_AUDIO := false
+TARGET_BOOTLOADER_BOARD_NAME := inc
 
-BOARD_KERNEL_CMDLINE := no_console_suspend=1
-BOARD_KERNEL_BASE := 0x20000000
+BOARD_KERNEL_CMDLINE := no_console_suspend=1 console=null
 
 BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_BCM := true
+
+BOARD_USES_GENERIC_AUDIO := false
+
+BOARD_KERNEL_BASE := 0x20000000
 
 BOARD_VENDOR_QCOM_AMSS_VERSION := 3200
 
@@ -36,7 +39,7 @@ BOARD_VENDOR_USE_AKMD := akm8973
 BOARD_EGL_CFG := device/htc/inc/egl.cfg
 
 BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00280000
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00480000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00500000
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x0f800000   # limited so we enforce room to grow
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x09500000
 BOARD_FLASH_BLOCK_SIZE := 131072
@@ -44,3 +47,4 @@ BOARD_FLASH_BLOCK_SIZE := 131072
 TARGET_RELEASETOOLS_EXTENSIONS := device/htc/common
 
 BOARD_GPS_LIBRARIES := libgps
+USE_CAMERA_STUB := false
